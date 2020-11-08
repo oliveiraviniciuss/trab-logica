18c8f494cc56e7dcfcdc2710ee91484629eaddb9
Vinícius de Oliveira <vi.o@hotmail.com>
2020-11-08 15:48:52 -0300

    subindo arquivo modificado do igor

--- controller.js
+++ controller.js
@@ -3,6 +3,9 @@ let numero_linhas;
 let numero_simbolos;
 let numero_conectivos;
 
+let matriz;
+let simbolos_cabecalho= [];
+
 function calcula_tamanho_tabela(){
     if(document.getElementById("simbolos").value.length == 0 || document.getElementById("conectivos").value.length == 0)
     {
@@ -11,50 +14,96 @@ function calcula_tamanho_tabela(){
     numero_simbolos = $('#simbolos').val() 
     numero_conectivos = $('#conectivos').val()
     numero_linhas = 2 ** numero_simbolos;
-    console.log(numero_linhas)
     numero_colunas = parseInt(numero_simbolos) + parseInt(numero_conectivos); 
 
     desenha_head_tabela(numero_colunas)
     desenha_linhas_tabela(numero_linhas, numero_colunas)
-    }
+}
 
 function desenha_head_tabela(numero_colunas){
     
-    let a;
+    let linha;
     for(let i = 0; i < numero_colunas; i++){
-        a += "<th contenteditable='true'>"  + "TESTE " + i + "</th>"    
+        linha += `<th contenteditable='true' id ="${i}"> TESTE ${i} </th>`;
     }
-    $("#header").append(a);
+    $("#header").append(linha);
+
 }
 
 
 function desenha_linhas_tabela(numero_linhas, numero_colunas){
-    let a;
+    let linha;
+
     for(let i = 0; i < numero_linhas; i++){
-        a = "<tr>";
+        linha = "<tr>";
         for(let j = 0; j < numero_colunas; j++){
-            a += "<td contenteditable='true' id=" + `"`+ i + "" +j + `"` + ">"  + "TESTE " + i +""+ j + "</td>"    
-         
+
+            // if (j === (numero_colunas - 1) ) 
+            //     linha += `<td contenteditable='true' id= "${i}${j}"> TRUE</td>`; 
+            // else
+                linha+= `<td contenteditable='true' id= "${i}${j}"> TESTE ${i}${j}</td>`; 
+          
         }
-        $("#table").append(a + "</tr>");
+        $("#table").append(linha + "</tr>");
     }
     
 }
 
 function converte_tabela_html_em_matriz(){
-    var matriz = new Array(numero_linhas); 
-    for (var i = 0; i < numero_linhas; i++) 
+    matriz = new Array(numero_linhas); 
+    for (let i = 0; i < numero_linhas; i++) 
         matriz[i] = new Array(numero_colunas);
-    console.log(numero_linhas)
-    console.log(numero_colunas)
+   
     for(let i = 0; i < numero_linhas; i++){
         for(let j = 0; j < numero_colunas; j++){
-            let teste = `#${i}${j}`
-            console.log($(teste))
-            //matriz[i][j] = $("'" + teste + "'")[0].innerHTML
-            matriz[i][j] = $(teste)[0].innerHTML
+            
+            // matriz[i][j] =   j === (numero_colunas - 1) ? 'TRUE' : $(`#${i}${j}`)[0].innerHTML
+            matriz[i][j] =  $(`#${i}${j}`)[0].innerHTML
+
         }
     }
     console.log(matriz)
 
+
+    for(let j = 0; j < numero_simbolos; j++){
+        simbolos_cabecalho.push($(`#${j}`)[0].innerHTML);
+    }
+
+}
+
+function gera_formulas_normais(){
+
+    gera_formula_normal_disjuntiva();
+}
+
+
+function gera_formula_normal_disjuntiva(){
+    
+    let linhas_true = [];
+
+    for(let i = 0; i < numero_colunas; i++){
+        if(matriz[i][numero_colunas-1].toLowerCase() === 'v'){
+            linhas_true.push(i)
+        }
+    }
+    console.log(linhas_true);
+
+    let formula = '';
+
+    for(let i of linhas_true){
+
+        formula+= '( ';
+
+        for(let j = 0; j < numero_simbolos; j++){
+
+            if(matriz[i][j] === 'TRUE')        
+                formula += ` ^ ${matriz[i][j]}` 
+            else
+                formula += `^ X${matriz[i][j]}`;
+        }
+
+        formula += ' ) V';
+    }
+
+    console.log(formula)
 }
