354d21223c6b1e2a51d0fe50fe27b9eff8d5aba4
Vinícius de Oliveira <vi.o@hotmail.com>
2020-11-07 23:46:30 -0300

    ajuste nomenclatura para montar a matriz com o numero de simbolos e numero de conectivos

--- controller.js
+++ controller.js
@@ -1,15 +1,18 @@
 let numero_colunas; 
 let numero_linhas;
-
+let numero_simbolos;
+let numero_conectivos;
 
 function calcula_tamanho_tabela(){
-    if(document.getElementById("col").value.length == 0 || document.getElementById("line").value.length == 0)
+    if(document.getElementById("simbolos").value.length == 0 || document.getElementById("conectivos").value.length == 0)
     {
         alert("empty")
     }
-    numero_colunas = $('#col').val() 
-    numero_linhas = $('#line').val()
-
+    numero_simbolos = $('#simbolos').val() 
+    numero_conectivos = $('#conectivos').val()
+    numero_linhas = 2 ** numero_simbolos;
+    console.log(numero_linhas)
+    numero_colunas = parseInt(numero_simbolos) + parseInt(numero_conectivos); 
 
     desenha_head_tabela(numero_colunas)
     desenha_linhas_tabela(numero_linhas, numero_colunas)
@@ -31,24 +34,23 @@ function desenha_linhas_tabela(numero_linhas, numero_colunas){
         a = "<tr>";
         for(let j = 0; j < numero_colunas; j++){
             a += "<td contenteditable='true' id=" + `"`+ i + "" +j + `"` + ">"  + "TESTE " + i +""+ j + "</td>"    
-                  
+         
         }
-        console.log(a)
-
         $("#table").append(a + "</tr>");
     }
     
 }
 
 function converte_tabela_html_em_matriz(){
-    var matriz = new Array(numero_colunas); 
-    for (var i = 0; i < numero_colunas; i++) 
-        matriz[i] = new Array(numero_linhas);
-        
+    var matriz = new Array(numero_linhas); 
+    for (var i = 0; i < numero_linhas; i++) 
+        matriz[i] = new Array(numero_colunas);
+    console.log(numero_linhas)
+    console.log(numero_colunas)
     for(let i = 0; i < numero_linhas; i++){
         for(let j = 0; j < numero_colunas; j++){
             let teste = `#${i}${j}`
-            console.log(teste)
+            console.log($(teste))
             //matriz[i][j] = $("'" + teste + "'")[0].innerHTML
             matriz[i][j] = $(teste)[0].innerHTML
         }
